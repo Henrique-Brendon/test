03560d4f6a9b84c10e3f5154d2d2e2d08287a3a4
Henrique Brendon <henriquebrendonti26@gmail.com>
2024-12-06 10:59:46 -0300

    Repository Layer

---
+++ src/main/java/com/henrique/crud/repositories/ListCodeRepository.java
@@ -0,0 +1,9 @@
+package com.henrique.crud.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.henrique.crud.entities.ListCode;
+
+public interface ListCodeRepository extends JpaRepository<ListCode, Long>{
+
+}
---
+++ src/main/java/com/henrique/crud/repositories/SectorRepository.java
@@ -0,0 +1,9 @@
+package com.henrique.crud.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.henrique.crud.entities.Sector;
+
+public interface SectorRepository extends JpaRepository<Sector, Long>{
+
+}
