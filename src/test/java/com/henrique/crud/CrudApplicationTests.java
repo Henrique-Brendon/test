5640196e24d6affd7cf22b3fea309fb26ec84192
Henrique Brendon <henriquebrendonti26@gmail.com>
2024-12-06 10:10:47 -0300

    Initial commit

---
+++ src/test/java/com/henrique/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
