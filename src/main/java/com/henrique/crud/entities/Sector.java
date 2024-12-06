d71b3c1defb063322a7e76f648ea4a2c68e0cdc6
Henrique Brendon <henriquebrendonti26@gmail.com>
2024-12-06 10:57:46 -0300

    Sector mapping functionality

--- src/main/java/com/henrique/crud/entities/Sector.java
+++ src/main/java/com/henrique/crud/entities/Sector.java
@@ -2,8 +2,11 @@ package com.henrique.crud.entities;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 import org.springframework.data.annotation.Id;
 
@@ -11,6 +14,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.henrique.crud.entities.enums.SectorType;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.OneToMany;
 
 @Entity
@@ -21,11 +26,32 @@ public class Sector implements Serializable {
 	@Id
 	private Long id;
 	
-	@JsonIgnore
-	@OneToMany(mappedBy = "listCode")
+	@Enumerated(EnumType.STRING)
 	private SectorType type;
 	
+	@JsonIgnore
+	@OneToMany(mappedBy = "listCode")
 	private List<Product> listProducts = new ArrayList<>();
+	
+	private static final Map<String, SectorType> PRODUCT_SECTORS = Map.of(
+	    "RX 560", SectorType.HARDWARE,
+	    "RX 570", SectorType.HARDWARE,
+	    "RX 580", SectorType.HARDWARE,
+	    "RX 590", SectorType.HARDWARE,
+	    "MOUSE REDRAGON", SectorType.PERIPHERALS,
+	    "KEYBOARD REDRAGON", SectorType.PERIPHERALS,
+	    "MICROONDAS", SectorType.ELETRONICS,
+	    "REFRIGERATOR", SectorType.ELETRONICS
+	);
+	
+	private static final EnumMap<SectorType, Long> SECTOR_TYPE_TO_ID = new EnumMap<>(SectorType.class);
+	static {
+	    SECTOR_TYPE_TO_ID.put(SectorType.DEFAULT, 0L);
+	    SECTOR_TYPE_TO_ID.put(SectorType.HARDWARE, 1L);
+	    SECTOR_TYPE_TO_ID.put(SectorType.ELETRONICS, 2L);
+	    SECTOR_TYPE_TO_ID.put(SectorType.PERIPHERALS, 3L);
+	    SECTOR_TYPE_TO_ID.put(SectorType.SMARTPHONE, 4L);
+	}
 	
 	public Sector(SectorType type) {
 		this.type = type;
@@ -50,7 +76,24 @@ public class Sector implements Serializable {
 	public List<Product> getListProducts() {
 		return listProducts;
 	}
+	
 
+	public Sector mapSector(String productName) {
+	    return Optional.ofNullable(PRODUCT_SECTORS.get(productName))
+	        .map(this::createSector)
+	        .orElseGet(this::getDefaultSector);
+	}
+
+
+	private Sector createSector(SectorType sectorType) {
+	    Sector sector = new Sector(sectorType);
+	    sector.setId(SECTOR_TYPE_TO_ID.get(sectorType));
+	    return sector;
+	}
+	
+	private Sector getDefaultSector() {
+	    return new Sector(SectorType.DEFAULT);
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
